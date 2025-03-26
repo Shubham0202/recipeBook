05151fa694a29b7f13f53b57718bf200f97d918a
Shubh <91457900+Shubham0202@users.noreply.github.com>
2025-03-26 23:34:02 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ app.use(express.static('public'));
 app.get('/',(req,res)=>{
 res.render("index");
 })
-app.listen(3001);
+app.listen(3001,()=>{
+    console.log("Server is running on port http://localhost:3001");
+});
